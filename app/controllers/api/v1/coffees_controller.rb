33f163c1c374f44eeee098e1cae920333caaaa71
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 14:12:20 -0400

    protect from forgery with null session in coffees and reviews controllers

--- app/controllers/api/v1/coffees_controller.rb
+++ app/controllers/api/v1/coffees_controller.rb
@@ -1,6 +1,8 @@
 module Api
     module V1
         class CoffeesController < ApplicationController
+            protect_from_forgery with: :null_session
+
             def index
                 coffees = Coffee.all
 
