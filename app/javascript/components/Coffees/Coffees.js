efc6244c3a65285db176788510bbbb576f835905
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 14:50:16 -0400

    installed axios and mapped out key value

--- app/javascript/components/Coffees/Coffees.js
+++ app/javascript/components/Coffees/Coffees.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Coffees = () => {
-    return <div>This is the Coffees#index view for our app</div>
+    const [coffees, setCoffees] = useState([])
+
+    useEffect(() => {
+        // Get all of our coffees from api
+        // Update coffees in our state
+
+        axios.get('/api/v1/coffees.json')
+        .then( resp => {
+            setCoffees(resp.data.data)
+        })
+        .catch( resp => console.log(resp) )
+    }, [coffees.length])
+
+    const list = coffees.map( item => {
+        return (
+            <li key={item.attributes.name}>{item.attributes.name}</li>
+        )
+    })
+
+    return (
+        <Fragment>
+            <div>This is the Coffees#index view for our app</div>
+            <ul>{list}</ul>
+        </Fragment>
+    )
 }
 
 export default Coffees
