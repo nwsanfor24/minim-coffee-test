4c2263b90b477b9cc532cecf6789403da8f27942
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 14:09:05 -0400

    syntax error on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :airlines, param: :slug
+      resources :coffees, param: :slug
       resources :reviews, only: [:create, :destroy]
     end
   end
