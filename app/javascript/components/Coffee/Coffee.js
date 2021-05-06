34f74249f97fae87c28b47721331ad7531a1c1d8
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 16:06:10 -0400

    creating a review form as well as individual coffee and header forms

--- app/javascript/components/Coffee/Coffee.js
+++ app/javascript/components/Coffee/Coffee.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+import styled from "styled-components";
 
-const Coffee = () => {
-    return <div>This is the Coffees#show view for our app</div>
-}
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
 
-export default Coffee
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+
+    &:last-child {
+        background: #000;
+    }
+`
+
+const Main = styled.div`
+    padding-left: 50px;
+`
+
+const Coffee = (props) => {
+  const [coffee, setCoffee] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/coffees/${slug}`;
+
+    axios
+      .get(url)
+      .then((resp) => {
+        setCoffee(resp.data);
+        setLoaded(true);
+      })
+      .catch((resp) => console.log(resp));
+  }, []);
+
+  return (
+    <Wrapper>
+      <Column>
+        <Main>
+          {loaded && (
+            <Header
+              attributes={coffee.data.attributes}
+              review={coffee.included}
+            />
+          )}
+          <div className="reviews"></div>
+        </Main>
+      </Column>
+      <Column>
+        <div className="review-form">[Review Form Goes Here.]</div>
+      </Column>
+    </Wrapper>
+  );
+};
+
+export default Coffee;
