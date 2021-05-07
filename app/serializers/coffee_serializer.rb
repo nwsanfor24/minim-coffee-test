b5695bf61dbb0f12c3585ec20ad3ca34a658c6b6
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 20:21:06 -0400

    updated serializers and mailers

--- app/serializers/coffee_serializer.rb
+++ app/serializers/coffee_serializer.rb
@@ -1,6 +1,10 @@
 class CoffeeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :slug, :avg_score
+  attributes :name, :image_url, :slug
 
+  attribute :avg_score do |object|
+    (object.avg_score.to_f / 100).to_f.round(2)
+  end
+  
   has_many :reviews
 end
