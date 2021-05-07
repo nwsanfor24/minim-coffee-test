f65d8e4e2c836e80b36589168b46dbac77893629
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 20:36:12 -0400

    added everything else to app.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Coffees from './Coffees/Coffees'
-import Coffee from './Coffee/Coffee'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Coffees from "./Coffees/Coffees";
+import Coffee from "./Coffee/Coffee";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Forgot from "./Auth/Password/Forgot";
+import Reset from "./Auth/Password/Reset";
+import Navbar from "./Navbar";
+import { AuthProvider } from "./AuthContext";
+import ProtectedRoute from "./ProtectedRoute";
+import UnprotectedRoute from "./UnprotectedRoute";
 
-const App = () => {
+class App extends Component {
+  render() {
     return (
+      <AuthProvider>
+        <Navbar />
         <Switch>
-            <Route exact path="/" component={Coffees}/>
-            <Route exact path="/coffees/:slug" component={Coffee}/>
+          <Route exact path="/" component={Coffees} />
+          <Route exact path="/coffees/:slug" component={Coffee} />
+          <UnprotectedRoute path="/login" component={Login} />
+          <UnprotectedRoute exact path="/register" component={Register} />
+          <UnprotectedRoute path="/forgot-password" component={Forgot} />
+          <UnprotectedRoute path="/reset-password" component={Reset} />
         </Switch>
-    )
+      </AuthProvider>
+    );
+  }
 }
 
-export default App
+export default App;
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -2,7 +2,7 @@ class ReviewSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :description, :score, :coffee_id
 
-  attribute :email do |object|
-    object&.user&.email
-  end
+  # attribute :email do |object|
+  #   object&.user&.email
+  # end
 end
