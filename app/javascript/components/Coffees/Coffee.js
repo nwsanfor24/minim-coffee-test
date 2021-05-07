4491de0404cb3203b1cc12395dc988915d4c8162
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-06 20:16:51 -0400

    changed styling and auth for coffee and coffees

--- app/javascript/components/Coffees/Coffee.js
+++ app/javascript/components/Coffees/Coffee.js
@@ -1,62 +1,66 @@
-import React from 'react'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Rating from '../Rating/Rating'
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import styled from "styled-components";
+import Rating from "../Rating/Rating";
 
 const Card = styled.div`
-    border: 1px solid #efefef;
-    background: #fff;
-    text-align: center;
-`
+  border: 1px solid #efefef;
+  background: #fff;
+`;
 
 const CoffeeLogo = styled.div`
+  height: 50px;
+  img {
+    height: 50px;
     width: 50px;
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
-    padding-top: 10px;
-
-    img {
-        height: 50px;
-        width: 50px;
-        border-radius: 100%;
-        border: 1px solid #efefef;
-    }
-`
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 100%;
+  }
+`;
 
 const CoffeeName = styled.div`
-    padding: 20px 0 10px 0;
-`
+  padding: 20px 0 10px 0;
+`;
 
 const LinkWrapper = styled.div`
-    margin: 30px 0 20px 0;
-    height: 50px;
-
-    a {
-        color: #fff;
-        background: #000;
-        border-radius: 4px;
-        padding: 10px 50px;
-        border: 1px solid #000;
-        width: 100%;
-        text-decoration: none;
+  margin: 30px 0 20px 0;
+  height: 50px;
+  a {
+    color: #fff;
+    background-color: #71b406;
+    border-radius: 4px;
+    padding: 10px 50px;
+    cursor: pointer;
+    border-radius: 3px;
+    border: 1px solid #71b406;
+    text-align: center;
+    line-height: 20px;
+    min-height: 40px;
+    margin: 7px;
+    font-weight: 600;
+    text-decoration: none;
+    width: 100%;
+    transition: ease-in-out 0.1s;
+    &:hover {
+      border-color: #619a07;
+      background: #619a07;
     }
-`
+  }
+`;
 
+const Coffee = ({ name, image_url, avg_score, slug, ...props }) => {
+  return (
+    <Card>
+      <CoffeeLogo>
+        <img src={image_url} alt={name} />
+      </CoffeeLogo>
+      <CoffeeName>{name}</CoffeeName>
+      <Rating score={avg_score} />
+      <LinkWrapper>
+        <Link to={"/coffees/" + slug}>View Coffees</Link>
+      </LinkWrapper>
+    </Card>
+  );
+};
 
-const Coffee = (props) => {
-    return (
-        <Card>
-            <CoffeeLogo>
-                <img src={props.attributes.image_url} alt={props.attributes.name}/>
-            </CoffeeLogo>
-            <CoffeeName>{props.attributes.name}</CoffeeName>
-            <Rating score={props.attributes.avg_score} />
-            <LinkWrapper>
-                <Link to={`/coffees/${props.attributes.slug}`}>View Coffees</Link>
-            </LinkWrapper>
-        </Card>
-    )
-}
-
-export default Coffee
+export default Coffee;
--- app/javascript/components/Coffees/Coffees.js
+++ app/javascript/components/Coffees/Coffees.js
@@ -1,66 +1,59 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import axios from 'axios'
-import Coffee from './Coffee'
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Coffee from "./Coffee";
+import Header from "./Header";
+import styled from "styled-components";
 
 const Home = styled.div`
-    text-align: center;
-    max-width: 1200px;
-    margin-left: auto;
-    margin-right: auto;
-`
-
-const Header = styled.div`
-    padding: 100px 100px 10px 100px;
-
-    h1 {
-        font-size: 42px;
-    }
-`
-
-const Subheader = styled.div`
-    font-weight: 300;
-    font-size: 26px;
-`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const Grid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 20px;
-    width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+  > div {
+    background-color: #fff;
+    border-radius: 5px;
     padding: 20px;
-`
-
+  }
+`;
 
 const Coffees = () => {
-    const [coffees, setCoffees] = useState([])
+  const [coffees, setCoffees] = useState([]);
 
-    useEffect(() => {
-        axios.get('/api/v1/coffees.json')
-        .then(resp => setCoffees(resp.data.data))
-        .catch(resp => console.log(resp))
-    }, [coffees.length])
+  useEffect(() => {
+    axios
+      .get("/api/v1/coffees.json")
+      .then((resp) => setCoffees(resp.data.data))
+      .catch((resp) => console.log(resp));
+  }, [coffees.length]);
 
-    const grid = coffees.map( item => {
-        return (
-            <Coffee 
-                key={item.attributes.name}
-                attributes={item.attributes}
-            />
-        )
-    })
+  const grid = coffees.map( (coffee, index) => {
+    const { name, image_url, slug, avg_score } = coffee.attributes
 
     return (
-        <Home>
-            <Header>
-                <h1>CoffeeReviews</h1>
-                <Subheader>Honest, unbiased coffee reviews.</Subheader>
-            </Header>
-            <Grid>
-                {grid}
-            </Grid>
-        </Home>
+      <Coffee 
+        key={index}
+        name={name}
+        image_url={image_url}
+        slug={slug}
+        average_score={avg_score}
+      />
     )
-}
+  })
 
-export default Coffees
+  return (
+    <Home>
+      <Header />
+      <Grid>{grid}</Grid>
+    </Home>
+  );
+};
+
+export default Coffees;
